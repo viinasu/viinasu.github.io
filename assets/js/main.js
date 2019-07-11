b82a8bac8e7f6d078fdb04af0b0afeb6042afb58
Ting Deng <maidenbelize@gmail.com>
2019-07-11 00:38:44 -0700

    added profile information and photos; dynamically add email and phone number to page; removed portfolio section;

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,6 +26,14 @@ jQuery(document).ready(function($) {
 
     });
 
+    let username = 'maidenbelize',
+        domain = 'gmail.com',
+        areaCode = '+1 (415) ',
+        phoneNumber = '891-9922';
+
+    $('#profile .email').html(`${username}@${domain}`);
+    $('#profile .phone').html(`${areaCode}${phoneNumber}`);
+
     /* ---------------------------------------------------------------------- */
     /* -------------------------- easyResponsiveTabs ------------------------ */
     /* ---------------------------------------------------------------------- */
@@ -159,77 +167,77 @@ jQuery(document).ready(function($) {
     /* ---------------------------------------------------------------------- */
 
     // Needed variables
-    var $contactform = $('#contactform'),
-            $success = ' Your message has been sent. Thank you!';
-
-    $contactform.submit(function() {
-        $.ajax({
-            type: "POST",
-            url: "php/contact.php",
-            data: $(this).serialize(),
-            success: function(msg)
-            {
-                var msg_error = msg.split(",");
-                var output_error = '';
-
-                if (msg_error.indexOf('error-message') != -1) {
-                    $("#contact-message").addClass("has-error");
-                    $("#contact-message").removeClass("has-success");
-                    output_error = 'Please enter your message.';
-                } else {
-                    $("#contact-message").addClass("has-success");
-                    $("#contact-message").removeClass("has-error");
-                }
-
-                if (msg_error.indexOf('error-email') != -1) {
-
-                    $("#contact-email").addClass("has-error");
-                    $("#contact-email").removeClass("has-success");
-                    output_error = 'Please enter valid e-mail.';
-                } else {
-                    $("#contact-email").addClass("has-success");
-                    $("#contact-email").removeClass("has-error");
-                }
-
-                if (msg_error.indexOf('error-name') != -1) {
-                    $("#contact-name").addClass("has-error");
-                    $("#contact-name").removeClass("has-success");
-                    output_error = 'Please enter your name.';
-                } else {
-                    $("#contact-name").addClass("has-success");
-                    $("#contact-name").removeClass("has-error");
-                }
-
-
-                if (msg == 'success') {
-
-                    response = '<div class="alert alert-success success-send">' +
-                            '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
-                            '<i class="glyphicon glyphicon-ok" style="margin-right: 5px;"></i> ' + $success
-                            + '</div>';
-
-
-                    $(".reset").trigger('click');
-                    $("#contact-name").removeClass("has-success");
-                    $("#contact-email").removeClass("has-success");
-                    $("#contact-message").removeClass("has-success");
-
-                } else {
-
-                    response = '<div class="alert alert-danger error-send">' +
-                            '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
-                            '<i class="glyphicon glyphicon-remove" style="margin-right: 5px;"></i> ' + output_error
-                            + '</div>';
-
-                }
-                // Hide any previous response text
-                $(".error-send,.success-send").remove();
-                // Show response message
-                $contactform.prepend(response);
-            }
-        });
-        return false;
-    });
+    // var $contactform = $('#contactform'),
+    //         $success = ' Your message has been sent. Thank you!';
+    //
+    // $contactform.submit(function() {
+    //     $.ajax({
+    //         type: "POST",
+    //         url: "php/contact.php",
+    //         data: $(this).serialize(),
+    //         success: function(msg)
+    //         {
+    //             var msg_error = msg.split(",");
+    //             var output_error = '';
+    //
+    //             if (msg_error.indexOf('error-message') != -1) {
+    //                 $("#contact-message").addClass("has-error");
+    //                 $("#contact-message").removeClass("has-success");
+    //                 output_error = 'Please enter your message.';
+    //             } else {
+    //                 $("#contact-message").addClass("has-success");
+    //                 $("#contact-message").removeClass("has-error");
+    //             }
+    //
+    //             if (msg_error.indexOf('error-email') != -1) {
+    //
+    //                 $("#contact-email").addClass("has-error");
+    //                 $("#contact-email").removeClass("has-success");
+    //                 output_error = 'Please enter valid e-mail.';
+    //             } else {
+    //                 $("#contact-email").addClass("has-success");
+    //                 $("#contact-email").removeClass("has-error");
+    //             }
+    //
+    //             if (msg_error.indexOf('error-name') != -1) {
+    //                 $("#contact-name").addClass("has-error");
+    //                 $("#contact-name").removeClass("has-success");
+    //                 output_error = 'Please enter your name.';
+    //             } else {
+    //                 $("#contact-name").addClass("has-success");
+    //                 $("#contact-name").removeClass("has-error");
+    //             }
+    //
+    //
+    //             if (msg == 'success') {
+    //
+    //                 response = '<div class="alert alert-success success-send">' +
+    //                         '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
+    //                         '<i class="glyphicon glyphicon-ok" style="margin-right: 5px;"></i> ' + $success
+    //                         + '</div>';
+    //
+    //
+    //                 $(".reset").trigger('click');
+    //                 $("#contact-name").removeClass("has-success");
+    //                 $("#contact-email").removeClass("has-success");
+    //                 $("#contact-message").removeClass("has-success");
+    //
+    //             } else {
+    //
+    //                 response = '<div class="alert alert-danger error-send">' +
+    //                         '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
+    //                         '<i class="glyphicon glyphicon-remove" style="margin-right: 5px;"></i> ' + output_error
+    //                         + '</div>';
+    //
+    //             }
+    //             // Hide any previous response text
+    //             $(".error-send,.success-send").remove();
+    //             // Show response message
+    //             $contactform.prepend(response);
+    //         }
+    //     });
+    //     return false;
+    // });
 
     /* ---------------------------------------------------------------------- */
     /* ----------------------------- Portfolio ------------------------------ */
